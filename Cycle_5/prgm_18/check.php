1c5074e8bb0deec2a69ffad37ca6bf73daa05211
MuraliKrishna <mailstomuralikrishna@gmail.com>
2024-11-26 22:23:44 +0530

    prgm_18 completed

---
+++ Cycle_5/prgm_18/check.php
@@ -0,0 +1,40 @@
+<?php
+ include("connect.php");
+ session_start();
+
+ if(isset($_POST['submit']))
+ {
+    $uname=$_POST['uname'];
+    $pword=$_POST['pword'];
+
+    $sql=mysqli_query($conn,"SELECT * FROM loginform WHERE uname='$uname' OR password='$pword'");
+
+ }
+
+while($row =mysqli_fetch_assoc($sql))
+{
+    $dbuname=$row['uname'];
+    $dbpword=$row['password'];
+}
+if(isset($dbuname)&& isset($dbpword))
+{
+  if($dbuname==$uname && $dbpword==$pword)
+  {
+    $_SESSION['login_user']=$uname;
+    header("Location:welcome.php");
+  }
+  elseif($dbuname==$uname && $dbpword!=$pword)
+  {
+    header("Location:main.php?msg=WRONG PASSWORD");
+  }
+  elseif($dbuname!=$uname && $dbpword==$pword)
+  {
+    header("Location:main.php?msg=Wrong Username");
+  }
+}
+else
+{
+    header("Location:main.php?msg=Wrong pasword and username");
+}
+
+?>
---
+++ Cycle_5/prgm_18/welcome.php
@@ -0,0 +1,18 @@
+<?php
+include("connect.php");
+session_start();
+?>
+
+<html>
+    <head>
+        <title>WELCOME-LOGIN</title>
+    </head>
+    <body>
+        <h1 align="center">WELCOME</h1>
+        <h1 align="center">
+            <?php
+               echo $_SESSION['login_user'];
+            ?>
+        </h1>
+    </body>
+</html>
