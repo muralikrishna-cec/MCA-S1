fb1594a0b8218d4b7eee5f7d4697e39225f45173
MuraliKrishna <mailstomuralikrishna@gmail.com>
2024-12-04 23:16:55 +0530

    co4 prgm3 complted

---
+++ Python/co4/prgm_3.py
@@ -0,0 +1,27 @@
+class Rectangle:
+    def __init__(self,l,b):
+        self.len=l
+        self.bd=b
+        
+    def display(self):
+        print("Length od reactangle is:",self.len)  
+        print("Breadth of rectangle is:",self.bd)
+        
+    def area(self):
+        return self.len*self.bd
+    
+    def __lt__(self,other):
+        if not isinstance(other,Rectangle):
+            raise TypeError("Value must be same")     
+        else:
+            return self.area() < other.area()
+        
+    
+rect1=Rectangle(4,5)
+rect2=Rectangle(6,6)
+
+if rect1 < rect2:
+    print("recatangle 2 is larger")
+    
+else:
+    print("rectangle 1 is larger")             
