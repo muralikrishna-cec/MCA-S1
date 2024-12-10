7aaf6a77e749a648103b99dd4e24d9cfcc6c8478
MuraliKrishna <mailstomuralikrishna@gmail.com>
2024-12-11 00:27:30 +0530

    modified

--- Python/CO5/prgm_5.py
+++ Python/CO5/prgm_5.py
@@ -1,3 +1,5 @@
+import csv
+
 data=[{'name':'murali','role':'student','college':'cec'},{'name':'Ramu','role':'teacher','college':'cec'},{'name':'raju','role':'lab','college':'cec'}]
 
 with open("/var/www/html/Web_programming/Python/CO5/dict.csv",'w') as file:
@@ -6,3 +8,9 @@ with open("/var/www/html/Web_programming/Python/CO5/dict.csv",'w') as file:
     for i in data:
         file.write(','.join(str(x) for x in i.values()))
         file.write('\n')
+  
+  
+with open("/var/www/html/Web_programming/Python/CO5/dict.csv",'r') as f:        
+    d=csv.reader(f)
+    for i in d:
+        print(i)   
